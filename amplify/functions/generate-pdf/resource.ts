78a176d84e646dc6832264bb6de5e1dc92128748
JulianOcampo <juliancamilocampo@gmail.com>
2025-08-15 00:15:34 -0500

    Implement code changes to enhance functionality and improve performance

--- amplify/functions/generate-pdf/resource.ts
+++ amplify/functions/generate-pdf/resource.ts
@@ -6,6 +6,9 @@ export const generatePdf = defineFunction({
   runtime: 20,
   timeoutSeconds: 360,
   memoryMB: 2048,
+  bundling: {
+    minify: false
+  },
   layers: {
     "layer-puppeteer-utils":
       "arn:aws:lambda:us-east-2:018889390014:layer:layer-puppeteer-utils:1",
