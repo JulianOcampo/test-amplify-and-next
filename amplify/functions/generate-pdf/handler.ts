cccdc0926000deee058435deb79d49f7ad6fed87
JulianOcampo <juliancamilocampo@gmail.com>
2025-08-15 00:33:10 -0500

    fix: update package-lock.json to mark extraneous dependencies as dev

--- amplify/functions/generate-pdf/handler.ts
+++ amplify/functions/generate-pdf/handler.ts
@@ -1,23 +1,15 @@
-import puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium";
 import { Schema } from "../../data/resource";
+import { generatePdfFromHtml } from "/opt/nodejs/utils"; 
 
 export const handler: Schema["generatePdf"]["functionHandler"] = async (event) => {
-  const html = event.arguments.html;
-  if (!html) throw new Error("Missing html");
+  if (!event.arguments.html) throw new Error("Missing html");
 
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath(),
-    headless: true,
-    ignoreHTTPSErrors: true
+  return await new Promise<string>(async (resolve, reject) => {
+    try {
+      const pdfBuffer = await generatePdfFromHtml(event.arguments.html);
+      resolve(pdfBuffer.toString("base64")); // Retorna el PDF en base64
+    } catch (err) {
+      reject(err);
+    }
   });
-
-  const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: "domcontentloaded" });
-
-  const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
-  await browser.close();
-
-  return Buffer.from(pdfBuffer).toString("base64");
 };
--- amplify/functions/generate-pdf/resource.ts
+++ amplify/functions/generate-pdf/resource.ts
@@ -7,10 +7,11 @@ export const generatePdf = defineFunction({
   timeoutSeconds: 360,
   memoryMB: 2048,
   bundling: {
-    minify: false
+    minify: false,
   },
   layers: {
     "layer-puppeteer-utils":
       "arn:aws:lambda:us-east-2:018889390014:layer:layer-puppeteer-utils:1",
   },
+  
 });
