7889c7ba2eb800e56c31abca84d13b4527110186
JulianOcampo <juliancamilocampo@gmail.com>
2025-08-14 22:11:56 -0500

    refactor: update say-hello function to include a request ID using UUID

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,8 +1,8 @@
 import type { Schema } from "../../data/resource"
-
+import { v4 as uuidv4 } from "uuid";
 export const handler: Schema["sayHello"]["functionHandler"] = async (event) => {
   // arguments typed from `.arguments()`
   const { name } = event.arguments
   // return typed from `.returns()`
-  return `Hello, ${name}!1`
+  return `Hello, ${name}! Your request ID is ${uuidv4()}`
 }
